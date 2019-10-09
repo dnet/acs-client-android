69bdf4cf4596b97ade9178285c80facb910b30b6
András Veres-Szentkirályi <vsza@vsza.hu>
2019-10-09 14:15:07 +0200

    removed unused imports

--- app/src/main/java/hu/vsza/androidclipboardsync/RegisterActivity.kt
+++ app/src/main/java/hu/vsza/androidclipboardsync/RegisterActivity.kt
@@ -1,7 +1,5 @@
 package hu.vsza.androidclipboardsync
 
-import android.content.Context
-import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
